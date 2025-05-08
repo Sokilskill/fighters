753f7443290a1438637e68728d75936f507216d3
sokilskills <vprovorotov@amil.com>
2025-05-08 22:48:42 +0300

    feat/added winner modal after fight completion
    - The `fight` function in `arena.js` is now awaited, ensuring the modal is shown only after the fight promise resolves.
    - `showWinnerModal` function is called with the winning fighter object to display the modal.

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,9 @@
 import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
+    showModal({
+        title: 'Winner',
+        bodyElement: `The winner is ${fighter.name}`
+    });
     // call showModal function
 }
