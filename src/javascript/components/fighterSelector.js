bca185245183658bdc61c6a81a0db14cb5c3d9dc
sokilskills <vprovorotov@amil.com>
2025-05-08 11:02:01 +0300

    feat: Implement caching for fighter details
    Introduced a caching mechanism using a Map to store and retrieve fighter
    details, improving performance by avoiding redundant API calls.

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,11 +2,18 @@ import createElement from '../helpers/domHelper';
 import renderArena from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import fighterService from '../services/fightersService';
 
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
-    // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+    if (fighterDetailsMap.has(fighterId)) {
+        return fighterDetailsMap.get(fighterId);
+    }
+
+    const dataFighter = await fighterService.getFighterDetails(fighterId);
+    fighterDetailsMap.set(fighterId, dataFighter);
+    return dataFighter;
 }
 
 function startFight(selectedFighters) {
